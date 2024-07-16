5e632ff91f61b043aa0786a0e15fb83a150827a4
Jisu Kim <jisukim.tech@gmail.com>
2024-07-16 21:15:49 +0900

    [Bronze III] Title: 곱셈, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -2,11 +2,8 @@ from sys import stdin
 
 number1 = int(stdin.readline().rstrip())
 number2 = stdin.readline().rstrip()
-answer = 0
 
 for i in range(2, -1, -1):
-    current_result = number1 * int(number2[i])
-    answer += (current_result) * 10 ** (2 - i)
-    print(current_result)
+    print(number1 * int(number2[i]))
     
-print(answer)
+print(number1 * int(number2))
