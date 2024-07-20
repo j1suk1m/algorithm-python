c013a47fd3b862c53296529539f4f419828762b4
Jisu Kim <jisukim.tech@gmail.com>
2024-07-20 16:13:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 384 ms, Memory: 141576 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,32 @@
+from sys import stdin
+from collections import deque
+
+def bfs(node):
+    visited[node] = order = 1
+    queue = deque([node])
+    
+    while queue:
+        current = queue.popleft()
+        
+        for next in graph[current]:
+            if not visited[next]:
+                order += 1
+                visited[next] = order ### 방문 처리 + 방문 순서
+                queue.append(next)
+
+N, M, R = map(int, stdin.readline().rstrip().split())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, stdin.readline().rstrip().split())
+    
+    graph[u].append(v)
+    graph[v].append(u)
+
+### 인접 정점을 오름차순으로 방문하기 위한 정렬
+for edge in graph:
+    edge.sort()
+    
+bfs(R)
+print("\n".join(list(map(str, visited[1:]))))
