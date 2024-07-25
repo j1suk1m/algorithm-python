35b88a7adb94960ae1c2be1ccb5c3f1929a46856
Jisu Kim <jisukim.tech@gmail.com>
2024-07-26 04:22:58 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -2,10 +2,7 @@ from sys import stdin
 
 N = int(stdin.readline().rstrip())
 
-for line in range(1, 2 * N):
-    if line <= N:
-        print(" " * (N - line), end="")
-        print("*" * (2 * line - 1))
-    else:
-        print(" " * (line - N), end="")
-        print("*" * (4 * N - 2 * line - 1))
+for line in range(1, N + 1):
+    print(" " * (N - line) + "*" * (2 * line - 1))
+for line in range(1, N):
+    print(" " * line + "*" * (2 * N - 1 - 2 * line))
