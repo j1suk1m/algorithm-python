074fbfe5f619d15102ac487e6e8514590334f0f4
Jisu Kim <jisukim.tech@gmail.com>
2024-07-29 22:42:10 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 452 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,29 @@
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(10 ** 5 * 2)
+
+def dfs(node):
+    global order
+    order += 1
+    visited[node] = order
+    
+    for next in graph[node]:
+        if not visited[next]:
+            dfs(next)
+    
+N, M, R = map(int, stdin.readline().rstrip().split())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+order = 0
+
+for _ in range(M):
+    u, v = map(int, stdin.readline().rstrip().split())
+    
+    graph[u].append(v)
+    graph[v].append(u)
+    
+for edge in graph:
+    edge.sort()
+    
+dfs(R)
+print("\n".join(list(map(str, visited[1:]))))
