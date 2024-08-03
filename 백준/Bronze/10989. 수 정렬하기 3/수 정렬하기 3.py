c4966605c0b019470eccb5eef767f89485c08193
Jisu Kim <jisukim.tech@gmail.com>
2024-08-03 15:15:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2208 ms, Memory: 126560 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,19 +1,13 @@
 from sys import stdin
-from collections import defaultdict
 
 N = int(stdin.readline().rstrip())
-number_dict = defaultdict(int) ### 기본값 0으로 설정
+numbers = [0] * 10001
 
 for _ in range(N):
     number = int(stdin.readline().rstrip())
-    number_dict[number] += 1
+    numbers[number] += 1
     
 for number in range(1, 10001):
-    if N <= 0:
-        break
-    
-    if number_dict[number] > 0:
-        for _ in range(number_dict[number]):
+    if numbers[number] > 0:
+        for _ in range(numbers[number]):
             print(number)
-    
-        N -= 1
