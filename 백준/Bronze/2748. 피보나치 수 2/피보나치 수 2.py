23ffc550f340d7ee2bf34270b04018a25080b9df
Jisu Kim <jisukim.tech@gmail.com>
2024-08-09 13:21:08 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+def fibonacci(number: int) -> int:
+    store = [0] * 91
+    store[1] = 1
+    
+    for i in range(2, number + 1):
+        store[i] = store[i - 1] + store[i - 2]
+        
+    return store[number]
+    
+n = int(stdin.readline().rstrip())
+
+if n <= 1:
+    print(n)
+else:
+    print(fibonacci(n))
