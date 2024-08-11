a3356bf45fd68f87bbabe4ef861fc563025820ae
Jisu Kim <jisukim.tech@gmail.com>
2024-08-11 18:27:33 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 104 ms, Memory: 116052 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -1,23 +1,15 @@
 from sys import stdin
 
-def dynamic_programming(n: int, m: int) -> int:
-    dxs = [0, -1, -1]
-    dys = [-1, -1, 0]
+def dynamic_programming(n: int, m: int):
     dp = [[0] * (m + 1) for _ in range(n + 1)]
     
     dp[1][1] = 1
 
     for x in range(1, n + 1):
         for y in range(1, m + 1):
-            for dx, dy in zip(dxs, dys):
-                bx = x + dx 
-                by = y + dy
-                
-                if (1 <= bx <= n and 1 <= by <= m):
-                    dp[x][y] += (dp[bx][by] % 1000000007)
-                    
-            dp[x][y] %= 1000000007
-                    
+            if not (x == 1 and y == 1):
+                dp[x][y] = (dp[x][y - 1] + dp[x - 1][y - 1] + dp[x - 1][y]) % 1000000007
+
     return dp[n][m]
 
 n, m = map(int, stdin.readline().rstrip().split())
