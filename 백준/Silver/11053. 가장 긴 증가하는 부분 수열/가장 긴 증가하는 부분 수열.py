54504ab0255e4ca508e0cfeca2b7de3bf3623011
Jisu Kim <jisukim.tech@gmail.com>
2024-08-15 11:15:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -8,8 +8,8 @@ if N == 1:
 else:
     dp = [1] * (N + 1) ### dp[idx] == idx를 수열의 최댓값으로 갖는 수열의 길이
     
-    for current in range(1, N + 1):
-        for rest in range(1, current + 1):
+    for current in range(2, N + 1):
+        for rest in range(1, current):
             if A[current] > A[rest]:
                 dp[current] = max(dp[current], dp[rest] + 1) 
     
