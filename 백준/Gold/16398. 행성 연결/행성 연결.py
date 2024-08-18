e254d93e9330130fa880311a86dc05c4809e6cdd
Jisu Kim <jisukim.tech@gmail.com>
2024-08-18 14:08:03 +0900

    [Gold IV] Title: 행성 연결, Time: 1204 ms, Memory: 207768 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -0,0 +1,40 @@
+from sys import stdin
+
+input = lambda: stdin.readline().rstrip()
+
+### 입력: 노드 번호
+### 출력: 해당 노드의 루트 노드 번호
+def find_root(node: int) -> int:
+    if root[node] != node:
+        root[node] = find_root(root[node])
+        
+    return root[node]
+
+N = int(input())
+graph = []
+cost = [[0]] + [([0] + list(map(int, input().split()))) for _ in range(N)]
+root = [node for node in range(N + 1)]
+answer = 0
+
+### 연결 관계 저장
+for x in range(1, N):
+    for y in range(x + 1, N + 1):
+        graph.append((x, y, cost[x][y]))
+
+### 비용에 대하여 오름차순 정렬      
+graph = sorted(graph, key=lambda x: x[2])
+
+for node1, node2, cost in graph:
+    root1 = find_root(node1)
+    root2 = find_root(node2)
+    
+    if root1 == root2: ### 사이클 발견
+        continue
+    elif root1 < root2:
+        root[root2] = root1
+    else:
+        root[root1] = root2
+        
+    answer += cost
+        
+print(answer)
