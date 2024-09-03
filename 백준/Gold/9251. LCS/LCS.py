adf3c54056e62267a7ef5b7e27728be1b8329cf0
Jisu Kim <jisukim.tech@gmail.com>
2024-09-03 17:03:32 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 118220 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+input = lambda: stdin.readline().rstrip()
+
+string1 = "_" + input()
+string2 = "_" + input()
+length1 = len(string1)
+length2 = len(string2)
+dp = [[0] * length2 for _ in range(length1)]
+answer = 0
+
+### 최장 공통 부분 수열
+for index1 in range(1, length1):
+    for index2 in range(1, length2):
+        char1 = string1[index1]
+        char2 = string2[index2]
+        
+        if char1 == char2:
+            dp[index1][index2] = dp[index1 - 1][index2 - 1] + 1
+        else:
+            dp[index1][index2] = max(dp[index1 - 1][index2], dp[index1][index2 - 1])
+            
+        answer = max(answer, dp[index1][index2])
+        
+print(answer)
