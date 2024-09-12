4aa91fff4d12717386286d0a90604b494da67649
Jisu Kim <jisukim.tech@gmail.com>
2024-09-12 19:44:10 +0900

    [Silver III] Title: N과 M (3), Time: 336 ms, Memory: 114412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -4,17 +4,12 @@ input = lambda: stdin.readline().rstrip()
 
 def back_tracking(sequence: str):
     if len(sequence) == M:
-        sequences.append(sequence)
         print(" ".join(sequence))
         return
     
     for number in range(1, N + 1):
-        if (len(sequence) == M - 1 and len(sequences) > 0 and int(sequence + str(number)) <= int(sequences[-1])):
-            continue
-
         back_tracking(sequence + str(number))
     
 N, M = map(int, input().split()) 
-sequences = []
 
 back_tracking("")
