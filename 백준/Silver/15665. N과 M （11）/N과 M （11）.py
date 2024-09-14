5c788b91e11deb8486a9202d677eb15cdfae85e4
Jisu Kim <jisukim.tech@gmail.com>
2024-09-14 19:49:35 +0900

    [Silver II] Title: N과 M (11), Time: 328 ms, Memory: 128076 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+input = lambda: stdin.readline().rstrip()
+
+### 백트래킹 알고리즘
+def back_tracking(count: int):
+    if count == M:
+        sequence = list(map(lambda index: numbers[index], sequence_indexes))
+        print(*sequence)
+        return
+    
+    ### 중복 수열 제거
+    previous = 0
+    
+    for index in range(N):
+        current = numbers[index]
+        
+        if (current != previous):
+            previous = current
+            sequence_indexes[count] = index
+            back_tracking(count + 1)
+            sequence_indexes[count] = -1
+
+            
+N, M = map(int, input().split())
+numbers = sorted(list(map(int, input().split())))
+sequence_indexes = [-1] * M
+
+back_tracking(0)
