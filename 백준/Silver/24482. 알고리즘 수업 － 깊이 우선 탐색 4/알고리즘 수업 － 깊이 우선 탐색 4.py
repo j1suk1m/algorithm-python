c29484f73245eb857782154241ab76e607a0edc2
Jisu Kim <jisukim.tech@gmail.com>
2024-10-05 23:28:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 628 ms, Memory: 70380 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,32 @@
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(10 ** 6)
+input = lambda: stdin.readline().rstrip()
+
+### 깊이 우선 탐색 알고리즘
+def dfs(node: int, depth: int):
+    visited[node] = depth ### 방문 여부와 노드 깊이를 동시에 저장
+    
+    for next in graph[node]:
+        if visited[next] == -1: ### 방문하지 않은 경우
+            dfs(next, depth + 1)
+    
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [-1] * (N + 1)
+
+### 인접 정점 정보 저장
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+### 인접 정점을 내림차순으로 방문하기 위한 정렬
+for edge in graph:
+    edge.sort(reverse=True)
+    
+### 깊이 우선 탐색
+dfs(R, 0)
+
+for depth in visited[1:]:
+    print(depth)
