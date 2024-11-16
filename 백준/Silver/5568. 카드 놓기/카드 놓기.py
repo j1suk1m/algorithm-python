b8850be8e89c40d503aee59b4189b392dab42dd3
Jisu Kim <jisukim.tech@gmail.com>
2024-11-16 14:36:53 +0900

    [Silver IV] Title: 카드 놓기, Time: 112 ms, Memory: 110724 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -2,31 +2,26 @@ from sys import stdin
 
 input = lambda: stdin.readline().rstrip()
 
-def back_tracking():
-    global count
-    
-    if len(selected_card_indices) == K:
-        number = "".join(list(map(lambda index: str(cards[index]), selected_card_indices)))
-        
-        if number not in answer:
-            count += 1
-            answer.append(number)
-            
+def back_tracking(selected_cards: list, count: int):
+    if count == K: ### 총 K개의 카드를 선택한 경우
+        number = "".join(map(str, selected_cards)) ### 카드로 만들 수 있는 정수
+        numbers.add(number) ### 정수 집합에 추가
         return
-    
-    for index in range(N):
-        if index not in selected_card_indices:
-            selected_card_indices.append(index)
-            back_tracking()
-            selected_card_indices.pop()
-    
+
+    for idx in range(len(cards)):
+        if not visited[idx]:
+            visited[idx] = 1
+            back_tracking(selected_cards + [cards[idx]], count + 1)
+            visited[idx] = 0
+
 N = int(input())
 K = int(input())
 cards = list(int(input()) for _ in range(N))
-selected_card_indices = []
-answer = []
-count = 0
 
-back_tracking()
+selected_cards = []
+visited = [0] * N
+numbers = set() ### 중복 제거
 
-print(count)
+back_tracking(selected_cards, 0)
+
+print(len(numbers))
