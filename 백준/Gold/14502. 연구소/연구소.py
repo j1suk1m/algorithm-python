2d9a52424cbe71dc5ba1d99d595d534462e5a493
Jisu Kim <jisukim.tech@gmail.com>
2024-11-17 03:28:16 +0900

    [Gold IV] Title: 연구소, Time: 2408 ms, Memory: 125808 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,59 +1,65 @@
 from sys import stdin
-from collections import deque
 from copy import deepcopy
+from collections import deque
 
-def bfs(x, y):
-    queue = deque([(x, y)])
-    graph_copy[x][y] = 1 
-    
+input = lambda: stdin.readline().rstrip()
+
+### 바이러스 전파
+def bfs(graph: list) -> int:
+    safety_area_count = len(spaces) - 3 ### 안전 영역의 개수
+    queue = deque(viruses) ### 바이러스 전체 좌표 삽입
+    x, y = viruses[0]
+    graph[x][y] = 1
+
     while queue:
         x, y = queue.popleft()
-        
+
         for dx, dy in zip(dxs, dys):
             nx = x + dx
             ny = y + dy
-            
-            if (0 <= nx and nx < N and 0 <= ny and ny < M and graph_copy[nx][ny] == 0):
+
+            if not (0 <= nx < N and 0 <= ny < M):
+                continue
+            elif graph[nx][ny] == 0:
                 queue.append((nx, ny))
-                graph_copy[nx][ny] = 1
-                
-N, M = map(int, stdin.readline().rstrip().split())
-graph = []
+                graph[nx][ny] = 1
+                safety_area_count -= 1
+
+    return safety_area_count
+
+### 벽 세우기
+def back_tracking(count):
+    global answer
+
+    if count == 3: ### 벽 3개 세우기 완료
+        safety_area_count = bfs(deepcopy(graph)) ### 바이러스 전파
+        answer = max(answer, safety_area_count)
+        return
+
+    for x, y in spaces:
+        if not graph[x][y]:
+            graph[x][y] = 1 ### 벽 세우기
+            back_tracking(count + 1)
+            graph[x][y] = 0 ### 벽 허물기
+
+N, M = map(int, input().split())
+graph = list(list(map(int, input().split())) for _ in range(N))
+
 dxs = [-1, 1, 0, 0]
 dys = [0, 0, -1, 1]
+spaces = []
+viruses = []
+space, virus = 0, 2
+answer = 0
 
-for _ in range(N):
-    graph.append(list(map(int, stdin.readline().rstrip().split())))
-    
-maximum_area = 0
+### 빈칸 좌표와 바이러스 좌표 각각 저장
+for x in range(N):
+    for y in range(M):
+        if graph[x][y] == space:
+            spaces.append((x, y))
+        elif graph[x][y] == virus:
+            viruses.append((x, y))
 
-for i in range(N * M):
-    for j in range(N * M):
-        for k in range(N * M):
-            if (i == j or j == k or k == i):
-                continue
-            
-            first_wall_x, first_wall_y = i // M, i % M
-            second_wall_x, second_wall_y = j // M, j % M
-            third_wall_x, third_wall_y = k // M, k % M
+back_tracking(0)
 
-            if (graph[first_wall_x][first_wall_y] != 0 or graph[second_wall_x][second_wall_y] != 0 or graph[third_wall_x][third_wall_y] != 0):
-                continue
-            
-            graph_copy = deepcopy(graph)
-            
-            graph_copy[first_wall_x][first_wall_y] = 1
-            graph_copy[second_wall_x][second_wall_y] = 1
-            graph_copy[third_wall_x][third_wall_y] = 1
-            
-            for x in range(N):
-                for y in range(M):
-                    if graph_copy[x][y] == 2:
-                        bfs(x, y)
-            
-            result = sum(row.count(0) for row in graph_copy)
-            
-            if result > maximum_area:
-                maximum_area = result
-
-print(maximum_area)  
+print(answer)
