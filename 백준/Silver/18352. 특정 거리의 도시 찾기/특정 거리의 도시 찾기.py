06e7d09e3fb16e00e58d617212003a30740fb6ce
Jisu Kim <jisukim.tech@gmail.com>
2024-11-17 02:44:54 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 688 ms, Memory: 154460 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,38 +1,40 @@
 from sys import stdin
 from collections import deque
 
-def bfs(start):
-    queue = deque([(start, 0)])
-    visited[start] = True
-    result = []
-    
+input = lambda: stdin.readline().rstrip()
+
+def bfs(node: int) -> list:
+    answer = []
+    queue = deque([(node, 0)])
+    visited[node] = 1
+
     while queue:
         node, distance = queue.popleft()
 
         if distance == K:
-            result.append(node)
-        else:
-            for next in graph[node]:
-                if not visited[next]:
-                    queue.append((next, distance + 1))
-                    visited[next] = True
-                
-    return result
-        
-N, M, K, X = map(int, stdin.readline().rstrip().split())
+            answer.append(node)
+        elif distance > K:
+            break
+
+        for next in graph[node]:
+            if not visited[next]:
+                queue.append((next, distance + 1))
+                visited[next] = 1
+
+    return sorted(answer)
+
+N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
+visited = [0] * (N + 1)
 
 for _ in range(M):
-    A, B = map(int, stdin.readline().rstrip().split())
+    A, B = map(int, input().split())
     graph[A].append(B)
-    
-result = bfs(X)
 
-if len(result) == 0:
-    print("-1")
+answer = bfs(X)
+
+if not answer:
+    print(-1)
 else:
-    result.sort()
-
-    for node in result:
+    for node in answer:
         print(node)
